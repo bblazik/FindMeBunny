8e663b08c9405dcf6961234dd6bb1e4930fe2e3f
bla bla <bartek.blazik@gmail.com>
2017-01-07 03:56:07 +0100

    Add reset text

--- FindMeBunny/Assets/MyScripts/GameController.cs
+++ FindMeBunny/Assets/MyScripts/GameController.cs
@@ -1,12 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 using System.Linq;
+using Prototype.NetworkLobby;
+using UnityEngine.Networking.Match;
 
 
 public class GameController : NetworkBehaviour {
     public static float counter = 10;
+    public static float counterToFinish = 5;
     public static GameObject[] PlayersList;
     public static bool SwitchOffLight = false;
     private static bool Called = false;
@@ -21,10 +25,25 @@ public class GameController : NetworkBehaviour {
     
     void Update()
     {
+        //Game is created already in queue so I need to check if game really started or not.
+        if (!GameObject.FindGameObjectWithTag("Lobby")
+                .GetComponent<Prototype.NetworkLobby.LobbyManager>()
+                .IsClientConnected()) return;
+
+        PlayersList = GameObject.FindGameObjectsWithTag("Player");
         RpcSwitchOffLights();
-        RpcDisplayCounter();
-        RpcSetLookingPlayer(GameObject.FindGameObjectsWithTag("Player").OrderBy(go => go.GetComponent<NetworkIdentity>().netId.Value).ToArray());
-        //print(PlayersList.Length);
+        RpcDisplayCounter(ref counter, "Time to hide: ");
+        RpcSetLookingPlayer(
+        GameObject.FindGameObjectsWithTag("Player")
+            .OrderBy(go => go.GetComponent<NetworkIdentity>().netId.Value)
+            .ToArray());
+
+        if (AllPlayersWereCatched() && counter < 0)
+        {
+            RpcDisplayCounter(ref counterToFinish, "Reset at: ");
+            if(calculateCounter(ref counterToFinish) < 0)
+                CmdReturnToLobby();
+        }
     }
     [Command] 
     void CmdSetLookingPlayer()
@@ -68,10 +87,7 @@ public class GameController : NetworkBehaviour {
     [RPC]
     void RpcSwitchOffLights()
     {
-        if (counter > 0)
-        {
-            counter -= Time.deltaTime;
-        }
+        calculateCounter(ref counter);
         if (counter < 0 && SwitchOffLight == false)
         {
             GameObject.FindGameObjectWithTag("MainLight").SetActive(false);
@@ -85,61 +101,36 @@ public class GameController : NetworkBehaviour {
         RpcReturnToLobby();
     }
 
+    float calculateCounter(ref float counter )
+    {
+        if (counter > 0)
+        {
+            counter -= Time.deltaTime;
+        }
+        return counter;
+    }
+
     [RPC]
     public void RpcReturnToLobby()
     {
-        StartCoroutine(ServerCountdownCoroutine());
         if (Called == false && isServer)
         {
             GameObject.FindGameObjectWithTag("Lobby").GetComponent<Prototype.NetworkLobby.LobbyManager>().SendReturnToLobby();
             Called = true;
         }
     }
-            
-    public IEnumerator ServerCountdownCoroutine()
-    {
-        float remainingTime = 5.0f;
-        int floorTime = Mathf.FloorToInt(remainingTime);
-
-        while (remainingTime > 0)
-        {
-            yield return null;
-
-            remainingTime -= Time.deltaTime;
-            int newFloorTime = Mathf.FloorToInt(remainingTime);
-
-            if (newFloorTime != floorTime)
-            {//to avoid flooding the network of message, we only send a notice to client when the number of plain seconds change.
-                floorTime = newFloorTime;
-
-                for (int i = 0; i < PlayersList.Length; ++i)
-                {
-                    if (PlayersList[i] != null)
-                    {//there is maxPlayer slots, so some could be == null, need to test it before accessing!
-                        //(PlayersList[i] as GameObject).RpcDisplay
-                    }
-                }
-            }
-        }
-        
-    }
     
     [RPC]
-    void RpcDisplayCounter()
-    {
+    void RpcDisplayCounter(ref float counter, String text)
+    { 
         
-        if (AllPlayersWereCatched() && counter < 0)
-        {
-            CmdReturnToLobby();
-        }
-
         if (PlayersList.Length != 0 && Called == false)
             foreach (GameObject p in PlayersList)
             {
                 if (counter > 0)
                 {
                     p.GetComponent<PlayerControll>().DisplayText.enabled = true;
-                    p.GetComponent<PlayerControll>().DisplayText.text = "time to hide: " + counter.ToString();
+                    p.GetComponent<PlayerControll>().DisplayText.text = text + counter.ToString();
                 }
                 else p.GetComponent<PlayerControll>().DisplayText.enabled = false;
 
